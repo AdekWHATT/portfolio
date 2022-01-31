34cdf2b714def922b7f3e70daab67954e1973edd
AdekWHATT <vyacheslaw.kilin@mail.ru>
2022-01-31 12:31:53 +0300

    1

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -11,20 +11,9 @@ function Resume() {
           <h2 className="top-title">Вячеслав Килин</h2>
           <ul>
             <li>
-              Core competencies in enterpirse content management, full-stack web
-              development, data architecture, and managing integrations.
+              Основные компетенции в управлении корпоративным контентом, комплексной веб-разработке, архитектуре данных и управлении интеграциями.
             </li>
           </ul>
-
-          <p class="mt-5">
-            <a href="https://www.linkedin.com/in/michaelgiddings/">
-              <img
-                src="https://img.icons8.com/fluent/48/000000/linkedin.png"
-                alt="inkedIn"
-              />
-            </a>
-          </p>
-
           <a
             href="https://github.com/fondofhats/react-portfolio/raw/main/docs/MichaelGiddingsResume.docx"
             class="link"
@@ -36,12 +25,10 @@ function Resume() {
 
       <div class="justify-content-center mt-5">
         <div>
-          <h2 className="top-title">Proficiencies</h2>
+          <h2 className="top-title">Мои знания</h2>
           <ul>
             <li>
-            SDL Tridion, Adobe Experience Manager (6.5, As a cloud Service), C#.NET, ASP.NET, JAVA,<br />
-            HTML, CSS (Bootstrap, HandleBars), JavaScript, jQuery, React.js, IndexedDB,<br />
-            Node.js, Express.js, SQL, Sequelize, MongoDB, Mongoose, NoSQL, APIs, Templating.
+             React, JavaScript, TypeScript, Redux, ES6, Axios, Material UI, Fetch, Firebase
             </li>
           </ul>
         </div>
